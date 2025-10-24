f12c61685e6254a51b01dd922d17fbea149de798
Emory Merryman <emory.merryman@gmail.com>
2025-10-24 12:13:14 -0400

    typo

--- flake.nix
+++ flake.nix
@@ -28,10 +28,10 @@
                                                                     echo 1 >&2
                                                                     mkdir --parents /mount
                                                                     echo 2 >&2
-                                                                    echo "nix build ${ package } >&2
+                                                                    echo "nix build ${ package }" >&2
                                                                     nix build ${ package }
                                                                     echo 3 >&2
-                                                                    echo nix eval ${ package } >&2
+                                                                    echo "nix eval ${ package }" >&2
                                                                     PACKAGE="$( nix eval ${ package } )" || failure
                                                                     echo 4 >&2
                                                                     find "$PACKAGE" -maxdepth 1 -mindepth 1 -name bin -exec ln --symbolic {} /mount \;
