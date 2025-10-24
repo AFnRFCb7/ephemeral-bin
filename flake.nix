9d85e2caa6b7805aeca3b82e2137351d4474bdc5
Emory Merryman <emory.merryman@gmail.com>
2025-10-24 13:11:15 -0400

    typo

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
                                                             runtimeInputs = [ coreutils findutils nix ( failure.implementation "d070b306" ) ] ;
                                                             text =
                                                                 ''
-                                                                    nix build ${ package } 2>&1
+                                                                    nix build ${ package } --out-link /scratch 2>&1
                                                                     PACKAGE="$( nix eval ${ package } --raw )" || failure
                                                                     find "$PACKAGE" -maxdepth 1 -mindepth 1 -name bin -exec ln --symbolic {} /mount \;
                                                                 '' ;
