cf8d790e29d4c5f36746d851bccc6689d93aca6b
Emory Merryman <emory.merryman@gmail.com>
2025-10-30 21:35:33 -0400

    next

--- flake.nix
+++ flake.nix
@@ -6,119 +6,69 @@
                 lib =
                     {
                         coreutils ,
-                        failure ,
                         garbage-collection-root ,
                         nix ,
-                        package ,
                         writeShellApplication
                     } :
                         let
                             implementation =
                                 {
-                                    init =
-                                        { resources , self } :
-                                            let
-                                                application =
-                                                    writeShellApplication
-                                                        {
-                                                            name = "init" ;
-                                                            runtimeInputs = [ coreutils nix ( failure.implementation "d070b306" ) ] ;
-                                                            text =
-                                                                ''
-                                                                    mkdir --parents ${ garbage-collection-root }
-                                                                    FILE="$( mktemp --dry-run ${ garbage-collection-root }/XXXXXXXX )" || failure mktemp --dry-run ${ garbage-collection-root }
-                                                                    nix build ${ package } --out-link "$FILE" 2>&1
-                                                                    PACKAGE="$( nix eval ${ package } --raw )" || failure nix eval ${ package }
-                                                                    ln --symbolic "$PACKAGE" /mount/ephemeral
-                                                                '' ;
-                                                        } ;
-                                                in "${ application }/bin/init" ;
-                                    targets = [ "ephemeral" ] ;
-                                } ;
+                                    package
+                                } :
+                                    {
+                                        init =
+                                            { resources , self } :
+                                                let
+                                                    application =
+                                                        writeShellApplication
+                                                            {
+                                                                name = "init" ;
+                                                                runtimeInputs = [ coreutils nix ( failure.implementation "d070b306" ) ] ;
+                                                                text =
+                                                                    ''
+                                                                        mkdir --parents ${ garbage-collection-root }
+                                                                        FILE="$( mktemp --dry-run ${ garbage-collection-root }/XXXXXXXX )" || failure mktemp --dry-run ${ garbage-collection-root }
+                                                                        nix build ${ package } --out-link "$FILE" 2>&1
+                                                                        PACKAGE="$( nix eval ${ package } --raw )" || failure nix eval ${ package }
+                                                                        ln --symbolic "$PACKAGE" /mount/ephemeral
+                                                                    '' ;
+                                                            } ;
+                                                    in "${ application }/bin/init" ;
+                                        targets = [ "ephemeral" ] ;
+                                    } ;
                             in
                                 {
                                     check =
                                         {
                                             expected-init ,
+                                            failure ,
                                             mkDerivation ,
+                                            package ,
                                             resources ? null ,
                                             self ? null
                                         } :
                                             mkDerivation
                                                 {
-                                                    installPhase =
-                                                        ''
-                                                            execute-test-init "$out"
-                                                            execute-test-targets "$out"
-                                                            execute-test-names "$out"
-                                                        '' ;
+                                                    installPhase = ''execute-test "$out"'' ;
                                                     name = "check" ;
                                                     nativeBuildInputs =
                                                         [
                                                             (
                                                                 writeShellApplication
                                                                     {
-                                                                        name = "execute-test-init" ;
-                                                                        runtimeInputs = [ coreutils ( failure.implementation "e75eb2bc" ) ] ;
+                                                                        name = "execute-test" ;
+                                                                        runtimeInputs = [ coreutils failure ] ;
                                                                         text =
                                                                             let
-                                                                                observed = implementation.init { resources = resources ; self = self ; } ;
+                                                                                init = instance { resources = resources ; self = self ; } ;
+                                                                                instance = implementation { package = package ; }
                                                                                 in
-                                                                                    if expected-init == observed then
-                                                                                        ''
-                                                                                            OUT="$1"
-                                                                                            touch "$OUT"
-                                                                                        ''
-                                                                                    else
-                                                                                        ''
-                                                                                            OUT="$1"
-                                                                                            touch "$OUT"
-                                                                                            failure "We expected ${ builtins.toString expected-init } but we observed ${ builtins.toString observed }"
-                                                                                        '' ;
-                                                                    }
-                                                            )
-                                                            (
-                                                                writeShellApplication
-                                                                    {
-                                                                        name = "execute-test-targets" ;
-                                                                        runtimeInputs = [ coreutils ( failure.implementation "1cbc4bb0" ) ] ;
-                                                                        text =
-                                                                            let
-                                                                                observed-targets = implementation.targets ;
-                                                                                in
-                                                                                    if [ "ephemeral" ] == observed-targets then
-                                                                                        ''
-                                                                                            OUT="$1"
-                                                                                            touch "$OUT"
-                                                                                        ''
-                                                                                    else
-                                                                                        ''
-                                                                                            OUT="$1"
-                                                                                            touch "$OUT"
-                                                                                            failure 'We expected the targets to be [ ephemeral ] but we observed ${ builtins.toJSON observed-targets }'
-                                                                                        '' ;
-                                                                    }
-                                                            )
-                                                            (
-                                                                writeShellApplication
-                                                                    {
-                                                                        name = "execute-test-names" ;
-                                                                        runtimeInputs = [ coreutils ( failure.implementation "1cbc4bb0" ) ] ;
-                                                                        text =
-                                                                            let
-                                                                                observed-names = builtins.attrNames implementation ;
-                                                                                in
-                                                                                    if [ "init" "targets" ] == observed-names then
-                                                                                        ''
-                                                                                            OUT="$1"
-                                                                                            touch "$OUT"
-                                                                                        ''
-                                                                                    else
-                                                                                        ''
-                                                                                            OUT="$1"
-                                                                                            touch "$OUT"
-                                                                                            failure 'We expected the targets to be [ init targets ] but we observed ${ builtins.toJSON observed-names }'
-                                                                                        '' ;
+                                                                                    ''
+                                                                                        OUT="$1"
+                                                                                        ${ if [ "init" "targets" ] != builtins.attrNames instance then ''failure ephemeral names "We expected the names to be init targets but we observed" "${ builtins.toJSON ( builtins.attrNames instance ) }"'' else "#" }
+                                                                                        ${ if expected != builtins.toString init then ''failure ephemeral init "We expected the init to be ${ expected } but we observed ${ builtins.toString init }"'' else "#" }
+                                                                                        ${ if [ "ephemeral" ] != instance.targets then ''failure ephemeral targets "We expected the targets to be ephemeral but we observed ${ builtins.toJSON instance.targets }"'' else "#" }
+                                                                                    '' ;
                                                                     }
                                                             )
                                                         ] ;
